6defcb347604b8cc90abd132edacdc78108fb442
Miguel Guinada <mguinada@gmail.com>
2016-03-01 11:07:52 +0000

    incorporate latest S3Url changes
    * bump version
    * place S3Url in the correct module

--- lib/mongo_mapper/exts/s3_url.rb
+++ lib/mongo_mapper/exts/s3_url.rb
@@ -1,39 +1,41 @@
 module MongoMapper
   module Exts
     module S3Url
-
-      # Adds 'path' key and updates it based on the value of 'url'. While 'url' stores the original S3 path,
-      # 'path' stores the path used by our S3 proxy, allowing us to serve S3 files securily.
-      #
-      # @note This module needs to be included after the key 'url' has been defined.
       module Proxy
-
         extend ActiveSupport::Concern
 
         included do
-
+          # Transitory phase of the managed downloads implementation.
+          # TODO: Set require to true on full implementation rollout
           key :path, String, required: false
+          key :public, Boolean, required: true, default: false
 
           alias_method :original_url=, :url=
+          alias_method :original_public=, :public=
 
           def url=(value)
             self.original_url = value
             update_path
+            value
           end
 
+          def public=(value)
+            self.original_public = value
+            update_path
+            value
+          end
         end
 
         private
 
-          def update_path
-            self.path = S3Url.origin_to_application_path(self.url)
-          end
-
+        def update_path
+          self.path = S3Url.origin_to_application_path(self.url, public: self.public)
+        end
       end
 
-      def self.origin_to_application_path(s3_url)
+      def self.origin_to_application_path(s3_url, opts={})
         return s3_url if s3_url.blank?
-        s3_url.gsub(ORIGIN_DOMAIN_NAME_PATTERN, 'files')
+        s3_url.gsub(ORIGIN_DOMAIN_NAME_PATTERN, opts.fetch(:public, false) ? 'public/files' : 'files')
       end
 
       def self.upload_params_to_s3_url(params)
@@ -44,8 +46,6 @@ module MongoMapper
 
         ORIGIN_DOMAIN_NAME         = "https://s3-eu-west-1.amazonaws.com"
         ORIGIN_DOMAIN_NAME_PATTERN = Regexp.new(ORIGIN_DOMAIN_NAME)
-
     end
-
   end
 end
--- spec/lib/mongo_mapper/exts/s3_url_spec.rb
+++ spec/lib/mongo_mapper/exts/s3_url_spec.rb
@@ -1,9 +1,11 @@
-describe MongoMapper::Exts::S3Url do
+require 'spec_helper'
 
-  let(:s3_hostname)      { "https://s3-eu-west-1.amazonaws.com" }
-  let(:file_path)        { "bucket.name/root/subfolder/file.pdf" }
-  let(:s3_url)           { "#{s3_hostname}/#{file_path}"}
-  let(:application_path) { "files/#{file_path}" }
+describe MongoMapper::Exts::S3Url do
+  let(:s3_hostname)             { "https://s3-eu-west-1.amazonaws.com" }
+  let(:file_path)               { "bucket.name/root/subfolder/file.pdf" }
+  let(:s3_url)                  { "#{s3_hostname}/#{file_path}"}
+  let(:application_path)        { "files/#{file_path}" }
+  let(:public_application_path) { "public/files/#{file_path}" }
 
   describe MongoMapper::Exts::S3Url::Proxy do
     class ProxyTestSubject
@@ -22,12 +24,20 @@ describe MongoMapper::Exts::S3Url do
 
       expect(doc.path).to eq application_path
     end
+
+    it '#path may be public' do
+      expect(ProxyTestSubject.new(url: s3_url, public: true).path).to eq public_application_path
+    end
   end
 
   describe 'URL conversions' do
     context 'AWS S3' do
-      it 'to application path' do
+      it 'to managed download path' do
         expect(MongoMapper::Exts::S3Url.origin_to_application_path(s3_url)).to eq application_path
+      end
+
+      it 'to managed public download path' do
+        expect(MongoMapper::Exts::S3Url.origin_to_application_path(s3_url, public: true)).to eq public_application_path
       end
 
       it 'nil to nil' do

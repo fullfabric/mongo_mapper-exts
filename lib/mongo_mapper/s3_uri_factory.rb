27f23c5ea4a924d026906f13e52671ca1e3d5fb7
Luis Correa d'Almeida <luis.ca@gmail.com>
2019-01-31 14:16:58 +0000

    support bucket name with dash

--- lib/mongo_mapper/s3_uri_factory.rb
+++ lib/mongo_mapper/s3_uri_factory.rb
@@ -3,10 +3,12 @@ module MongoMapper
 
     def self.build_path_from_url(url_on_s3, opts={})
       return url_on_s3 if url_on_s3.blank?
-      # match https://s3-eu-west-1.amazonaws.com/fullfabric.development/tbs and replace
+      # match https://s3-eu-west-1.amazonaws.com/fullfabric.env/tbs or
+      # https://fullfabric-env.s3-eu-west-1.amazonaws.com/tbs and replace
       # using this regex means we don't need to know the actual bucket/tenant
-      regex = Regexp.new("https://s3[-.].*amazonaws.com/[a-z\.\-]+/[a-z]+/")
-      url_on_s3.gsub(regex, opts.fetch(:public, false) ? 'public/files/' : 'files/')
+      url_on_s3.
+        gsub(Regexp.new("https://s3[-.]+.*amazonaws.com/[a-z\.\-]+/[a-z]+/"), opts.fetch(:public, false) ? 'public/files/' : 'files/').
+        gsub(Regexp.new("https://[a-z\-]+[-.]+s3[-.]+.*amazonaws.com/[a-z]+/"), opts.fetch(:public, false) ? 'public/files/' : 'files/')
     end
 
     def self.build_s3_url_from_http_params(params)
--- spec/lib/mongo_mapper/s3_uri_factory_spec.rb
+++ spec/lib/mongo_mapper/s3_uri_factory_spec.rb
@@ -1,10 +1,6 @@
 describe MongoMapper::S3UriFactory do
 
-  let(:s3_hostname)             { "https://s3-eu-west-1.amazonaws.com" }
-  let(:s3_hostname_alt)         { "https://s3.eu-central-1.amazonaws.com" }
-
-  let(:s3_file_path)            { "bucket-name/tenant/subfolder/file.pdf" }
-  let(:s3_url)                  { "#{s3_hostname}/#{s3_file_path}"}
+  let(:s3_url)                  { "https://s3-eu-west-1.amazonaws.com/bucket-name/tenant/subfolder/file.pdf"}
   let(:application_path)        { "files/subfolder/file.pdf" }
   let(:public_application_path) { "public/files/subfolder/file.pdf" }
 
@@ -13,12 +9,17 @@ describe MongoMapper::S3UriFactory do
     context 'creates download path' do
 
       # There are two types of AWS URLs: http://www.wryway.com/blog/aws-s3-url-styles/
-      it 'creates download path for https://s3-region.amazonaws.com' do
+      it 'creates download path for https://s3-eu-west-1.amazonaws.com/bucket-name/tenant/subfolder/file.pdf' do
         expect(described_class.build_path_from_url(s3_url)).to eq application_path
       end
 
-      it 'creates download path for https://s3.region.amazonaws.com' do
-        s3_url = "#{s3_hostname_alt}/#{s3_file_path}"
+      it 'creates download path for https://s3.eu-central-1.amazonaws.com/bucket-name/tenant/subfolder/file.pdf' do
+        s3_url = "https://s3.eu-central-1.amazonaws.com/bucket-name/tenant/subfolder/file.pdf"
+        expect(described_class.build_path_from_url(s3_url)).to eq application_path
+      end
+
+      it 'creates download path for https://bucket-name.s3.region.amazonaws.com' do
+        s3_url = "https://bucket-name.s3.eu-central-1.amazonaws.com/tenant/subfolder/file.pdf"
         expect(described_class.build_path_from_url(s3_url)).to eq application_path
       end
 
@@ -36,8 +37,7 @@ describe MongoMapper::S3UriFactory do
   describe '.build_s3_url_from_http_params' do
 
     it 'creates s3 url' do
-      s3_file_path_without_extension = s3_file_path[0..-5]
-      params = { url: s3_file_path_without_extension, format: 'pdf' }
+      params = { url: "bucket-name/tenant/subfolder/file", format: 'pdf' }
       expect(described_class.build_s3_url_from_http_params(params)).to eq s3_url
     end
 
